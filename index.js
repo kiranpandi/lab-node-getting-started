693a7323a30909403207596199347066bbcb8ef9
kiran <suriyakiran1@gmail.com>
2020-12-07 20:08:32 +0530

    Done

--- index.js
+++ index.js
@@ -1,26 +1,55 @@
 class SortedList {
   constructor() {
-  
+    this.items = [],
+    this.length = 0
   }
-  add() {
-   
+  add(item) {
+   this.items.push(item);
+   this.items.sort((a,b)=>(a-b));
+   this.length = this.length + 1;
   }
-  get() {
-
+  get(pos) {
+    if(this.items.indexOf(pos) == -1){
+      throw new Error('OutOfBounds');
+    }
+    else{
+      let ele = this.items[pos];
+      return ele;
+    }
   }
   max() {
-
+    if(this.items.length > 0){
+      return Math.max(...this.items);
+    }
+    else{
+      throw new Error('EmptySortedList');
+    }
   }
   min() {
-
+    if(this.items.length > 0){
+      return Math.min(...this.items);
+    }
+    else{
+      throw new Error('EmptySortedList');
+    }
   }
   avg() {
-    
-   
+    if(this.items.length > 0){
+      const tot = this.items.reduce( (tot,item) => tot+item,0 )
+      return tot/this.items.length;
+    }
+    else{
+      throw new Error('EmptySortedList');
+    }
   }
-
   sum() {
-  
+    if(this.items.length > 0){
+      const tot = this.items.reduce( (tot,item) => tot+item,0 )
+      return tot;
+    }
+    else{
+      return 0 ;
+    }
   }
   
 };
